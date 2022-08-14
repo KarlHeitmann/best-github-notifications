2e9aecf6f401ce963b99fb3f305d97a15c60fddd
Karl Heitmann <khf1988@gmail.com>
2022-08-14 18:40:15 -0400

    hello controller has the javascript logic, it's a little hacky

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
+async function fetchNotifications(token) {
+  const resp = await fetch('https://api.github.com/notifications', {
+      headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `token ${token}`
+      }
+    })
+  const data = await resp.json()
+  return data
+}
+
 export default class extends Controller {
+    static targets = [ "name", "output", "tok", "url" ]
+  /*
+    static targets = {
+      "name": Object,
+      "output": Object,
+      "tok": Object,
+      "url": Object,
+    }
+    */
+
+
   connect() {
-    this.element.textContent = "Hello World!"
+    const fetch_data = async () => {
+      const r = await fetchNotifications(this.data.get('token'))
+      console.log(r)
+    }
+    fetch_data()
+  }
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
   }
 }
